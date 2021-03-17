51412dd5049384375d332ddba79a86165abe4b08
sangwon <agbsw96@naver.com>
2021-03-17 11:07:53 +0900

    Add UserController [Update]

--- UserController.java
+++ UserController.java
@@ -7,4 +7,20 @@ public class UserController {
 
 		return "user/login");
 	}
+	
+	// 회원가입 페이지로 이동
+	@RequestMapping("/user/insert.do")
+	public String insert(){
+		logger.info("경로 : insert");
+
+		return "user/insert");
+	}
+	
+	// 회원수정 페이지로 이동
+	@RequestMapping("/user/update.do")
+	public String update(){
+		logger.info("경로 : update");
+
+		return "user/update");
+	}
 }
